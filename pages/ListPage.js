2a8b69d473c388ec8c1692c94ea5ca75d711c4cb
Aiting Zhou <zhou0248@algonquinlive.com>
2024-09-22 20:42:22 -0400

    save changes

--- pages/ListPage.js
+++ pages/ListPage.js
@@ -17,34 +17,51 @@ export default function ListPage({ navigation }) {
   return (
     <SafeAreaView>
       <View styles={styles.container}>
-        <SegmentedControl
-          values={["African Grey", "Cockatiel", "Parakeet", "Sun Conure"]}
-          selectedIndex={parrotIndex}
-          onChange={(event) => {
-            setParrotIndex(event.nativeEvent.selectedSegmentIndex);
-          }}
-          backgroundColor={"#ffe1e1"}
-        />
-        <Image
-          source={images[parrotIndex]}
-          style={{ width: 400, height: 400 }}
-        />
-        <FlatList
-          style={styles.itemContainer}
-          data={data}
-          renderItem={({ item }) => (
-            <View>
-              <Text style={styles.item}>{item.title}</Text>
-            </View>
-          )}
-          keyExtractor={(item) => item.id}
-        />
-        <Pressable
-          style={styles.btn}
-          onPress={() => navigation.navigate("Home")}
-        >
-          <Text style={styles.text}> Home Page</Text>
-        </Pressable>
+        <View styles={styles.segmentControl}>
+          <SegmentedControl
+            values={["African Grey", "Cockatiel", "Parakeet", "Sun Conure"]}
+            selectedIndex={parrotIndex}
+            onChange={(event) => {
+              setParrotIndex(event.nativeEvent.selectedSegmentIndex);
+            }}
+            backgroundColor={"#ffe1e1"}
+          />
+          <Image
+            source={images[parrotIndex]}
+            style={{
+              width: 250,
+              height: 250,
+              borderRadius: 20,
+              alignSelf: "center",
+              marginTop: 20,
+            }}
+          />
+        </View>
+        <View styles={styles.listItems}>
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <View>
+                <Text style={styles.item}>{item.title}</Text>
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+        <View styles={styles.btnGroup}>
+          <Pressable
+            style={styles.btn}
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Text style={styles.text}>Home Page</Text>
+          </Pressable>
+          <Pressable
+            style={styles.btn}
+            onPress={() => navigation.navigate("Cockatoo")}
+          >
+            <Text style={styles.text}>Cockatoo</Text>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -67,33 +84,43 @@ const data = [
   },
   { id: "2", title: "Parakeet" },
   { id: "3", title: "Sun Conure" },
+  { id: "4", title: "macaw" },
 ];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff3f3",
-    justifyContent: "flex-start",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  itemContainer: {
+  listItems: {
     marginTop: 15,
+    maxHeight: 250,
   },
   item: {
-    padding: 20,
+    padding: 15,
     fontSize: 20,
     backgroundColor: "#ffeeee",
     color: "#190808",
     margin: 5,
   },
+  btnGroup: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   btn: {
     backgroundColor: "#ffeeee",
-    borderColor: "#ffeeee",
+    borderColor: "#994444",
     borderRadius: 10,
+    borderWidth: 2,
     padding: 10,
-    marginTop: 15,
-    marginBottom: 15,
-    alignItems: "center",
-    justifyContent: "center",
+    width: 150,
+    textAlign: "center",
+    alignSelf: "center",
+    marginBottom: 10,
   },
   text: {
     fontSize: 16,
